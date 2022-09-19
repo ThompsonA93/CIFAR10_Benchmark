82d23a98ac7dc91b73a8054ae5b4a5bbe66b7a29
ThompsonA93 <thauer@edu.aau.at>
2022-09-19 14:00:03 +0200

    chore: cleaned and reconfigured, now needs training and results

--- src/config.py
+++ src/config.py
@@ -5,20 +5,20 @@ import platform
 # Training-Size
 # The CIFAR-10 dataset consists of 60000 32x32 colour images in 10 classes, with 6000 images per class. 
 # There are 50000 training images and 10000 test images. 
-num_train = 25000
-num_test  = 5000
+num_train = 12500
+num_test  = 2500
 num_classes = 10
 
 # Set parameters for CNN-Training
 batch_size = 32
-num_epochs = 10
+num_epochs = 8
 num_of_units = 256
 
 # True/False: Run hyper-parameter search via Keras-Tuner or GridSearchCV
 # NOTE: Takes ~8 hours for GridSearch and ~4 hours for Hyperband. Adjust variables as needed
 hyper_parameter_search = True
-# Keras-Tuner-only configurations
-hps_max_epochs = 25
+hps_max_epochs = 24
+hps_max_trials = 16
 
 # For echo operating system parameters
 os = platform.platform()
