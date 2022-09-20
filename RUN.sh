95980c4a6ef849bd359bdb28a30a8bd89cf4caf7
ThompsonA93 <thauer@edu.aau.at>
2022-09-20 08:28:14 +0200

    Cleaning & generating data

--- RUN.sh
+++ RUN.sh
@@ -6,6 +6,7 @@ for (( i=$start; i<=$end; i++ ))
 do   
     echo "[$i/$end] Script iteration"
     python3 src/01_CIFAR10_SKlearn.py
+    python3 src/02_CIFAR10_Keras.py
     echo ""
     echo ""
 done
