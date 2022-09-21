9500fbb40da1e108396927b309db7f3717ee5180
ThompsonA93 <thauer@edu.aau.at>
2022-09-21 13:18:47 +0200

    Hotfix on Runscript

--- RUN.ps1
+++ RUN.ps1
@@ -1,6 +1,6 @@
 for($i = 0; $i -lt 3; $i++){
     Write-Host "[$i] Script iteration" 
-    #python.exe .\src\01_CIFAR10_SKlearn.py
+    python.exe .\src\01_CIFAR10_SKlearn.py
     python.exe .\src\02_CIFAR10_Keras.py
     Write-Host ""
     Write-Host ""
